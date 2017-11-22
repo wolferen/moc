5868a8ab297798555e2a6f6e8e242cd21ea44f65
wolferen <veliar.master@gmail.com>
2017-11-23 01:38:22 +0200

    arhitecture changing

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'awesome_print'
 end
 
 group :development do
--- app/models/saved_weather.rb
+++ app/models/saved_weather.rb
@@ -1,2 +1,51 @@
 class SavedWeather < ApplicationRecord
+  OPENWEATHER = {
+    domain: "http://api.openweathermap.org",
+    path: "/data/2.5/weather",
+    APPID: "92fef38bd079859aea812af22a95f4de",
+    units: :metric
+  }.freeze
+
+  attr_accessor :location
+
+  before_create :get_weather
+
+  private
+
+  def get_weather
+    yahoo_response = Weather.lookup_by_location(
+      "#{location}, UA",
+      Weather::Units::CELSIUS
+    )
+    owm_response = get_openweather
+
+    self.yahoo_title = yahoo_response.title,
+    self.yahoo_temp = yahoo_response.condition.temp,
+    self.yahoo_text = yahoo_response.condition.text,
+    self.owm_min = owm_response['main']['temp_min'],
+    self.owm_max = owm_response['main']['temp_max'],
+    self.owm_cloud = owm_response['weather'][0]['description']
+  end
+
+  def get_openweather
+    params = {
+      APPID: OPENWEATHER[:APPID],
+      units: OPENWEATHER[:units],
+      q: "#{location},UA"
+    }
+    http_get(OPENWEATHER[:domain], OPENWEATHER[:path], params)
+  end
+
+  def http_get(domain,path,params)
+    require 'net/http'
+    require 'cgi'
+    response = Net::HTTP.get_response(
+      URI(
+        "#{domain}#{path}?".concat(
+          params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
+        )
+      )
+    ).body
+    JSON.parse(response)
+  end
 end
