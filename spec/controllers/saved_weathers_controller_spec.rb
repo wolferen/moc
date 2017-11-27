7eb9dfd3caed066a824e928923eec4c097440cb6
wolferen <veliar.master@gmail.com>
2017-11-28 00:40:31 +0200

    rspec tests

--- spec/controllers/saved_weathers_controller_spec.rb
+++ spec/controllers/saved_weathers_controller_spec.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe SavedWeathersController, type: :controller do
+  let(:user){ create(:user) }
+
+  before(:all) { Warden.test_reset! }
+  before(:each) { Warden.test_reset! }
+
+  describe 'get #index' do
+    it 'returns http success' do
+      sign_in(user)
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'create weather' do
+    it 'create weather' do
+      sign_in(user)
+      expect { post :create, params: {saved_weather: {location: "Kiev"}}, format: :js }.to change(SavedWeather, :count).by(1)
+    end
+  end
+
+  describe 'delete weather' do
+    it 'delete and render to index partial' do
+      sign_in(user)
+      saved_weather = create(:saved_weather, user: user)
+      expect { delete :destroy, params: {id: saved_weather.id}, format: :js }.to change(SavedWeather, :count).by(-1)
+    end
+  end
 end
--- spec/factories/saved_weathers.rb
+++ spec/factories/saved_weathers.rb
@@ -2,12 +2,7 @@
 
 FactoryBot.define do
   factory :saved_weather do
-    location Faker::Name.title
-    yahoo_title Faker::Name.title
-    yahoo_temp  Faker::Number.digit
-    yahoo_text  Faker::Job.title
-    owm_min  Faker::Number.digit
-    owm_max  Faker::Number.digit
-    owm_cloud Faker::Name.first_name
+    location "Kiev"
+    user
   end
 end
